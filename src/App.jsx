b6a26b9c5d34c20a3bfdef7233888137f2a1e6b9
Adwait5522 <adwaitkotewar25@gmail.com>
2025-08-09 14:02:22 +0530

    Background images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App.css";
+// import { useNavigate } from "react-router-dom";
+// import "./Components/Header";
+// import Header from "./Components/Header";
+// import Footer from "./Components/Footer";
+// function App() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <>
+//       <Header />
+//       <div className="app-container">
+//         <h1 className="title">Welcome to Complaint Portal</h1>
+//         <div className="button-group">
+//           <button
+//             className="btn btn-success"
+//             onClick={() => navigate("/user-login")}
+//           >
+//             Register a Complaint
+//           </button>
+//           <button
+//             className="btn btn-success"
+//             onClick={() => navigate("/officer-login")}
+//           >
+//             Officer Login
+//           </button>
+//           <button
+//             className="btn btn-success"
+//             onClick={() => navigate("/headquarter-login")}
+//           >
+//             Headquarter Login
+//           </button>
+
+
+        
+
+//         </div>
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { useNavigate } from "react-router-dom";
-import "./Components/Header";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+
+// Import background images
+import img1 from "./assets/indian-logo-satyamev-jayate.png";
+import img2 from "./assets/Supreme-Court-of-India.jpg";
+import img3 from "./assets/cbi.jpg";
+import img4 from "./assets/bombay-high-court.jpg";
+
 function App() {
   const navigate = useNavigate();
 
@@ -11,30 +63,38 @@ function App() {
     <>
       <Header />
       <div className="app-container">
-        <h1 className="title">Welcome to Complaint Portal</h1>
-        <div className="button-group">
-          <button
-            className="btn btn-success"
-            onClick={() => navigate("/user-login")}
-          >
-            Register a Complaint
-          </button>
-          <button
-            className="btn btn-success"
-            onClick={() => navigate("/officer-login")}
-          >
-            Officer Login
-          </button>
-          <button
-            className="btn btn-success"
-            onClick={() => navigate("/headquarter-login")}
-          >
-            Headquarter Login
-          </button>
 
+        {/* Background Slideshow */}
+        <div className="background-slideshow">
+          <img src={img1} alt="Satyamev Jayate" />
+          <img src={img2} alt="Supreme Court" />
+          <img src={img3} alt="CBI" />
+          <img src={img4} alt="Prime Minister of India" />
+        </div>
 
-        
-
+        {/* Foreground Content */}
+        <div className="app-content">
+          <h1 className="title">Welcome to Complaint Portal</h1>
+          <div className="button-group">
+            <button
+              className="btn btn-success"
+              onClick={() => navigate("/user-login")}
+            >
+              Register a Complaint
+            </button>
+            <button
+              className="btn btn-success"
+              onClick={() => navigate("/officer-login")}
+            >
+              Officer Login
+            </button>
+            <button
+              className="btn btn-success"
+              onClick={() => navigate("/headquarter-login")}
+            >
+              Headquarter Login
+            </button>
+          </div>
         </div>
       </div>
       <Footer />
